80a3f98daa690b15b317b53d4a9c878d35811b5f
tanakas-2021 <shugotanaka2020@gmail.com>
2025-07-20 17:33:04 +0900

    config.jsonをserver.pyで読み取るように修正

--- server.py
+++ server.py
@@ -1,12 +1,6 @@
 import socket
 import os
-
-# アドレスファミリ : socket.AF_INET
-# 通信方式 : SOCK_STREAM = TCP通信（信頼性あり、順番保証、コネクション型）
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = '127.0.0.1'
-server_port = 9001
-MAX_STORAGE = 4 * 1024 ** 4 # 4TB
+import json
 
 def get_storage_usage(dir_path:str) -> int:
     total = 0
@@ -16,12 +10,16 @@ def get_storage_usage(dir_path:str) -> int:
             total += os.path.getsize(fp)
     return total
 
-# mp4を保存するdirPath
-dir_path = 'storage'
-if not os.path.exists(dir_path):
-    os.makedirs(dir_path)
+with open('config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+server_address = config['server_address']
+server_port = config['server_port']
+max_storage = config['max_storage']
+dir_path = config['storage_dir']
 
-# socketにソケットファイルをバインドし、listenする
+# アドレスファミリ : socket.AF_INET
+# 通信方式 : SOCK_STREAM = TCP通信（信頼性あり、順番保証、コネクション型）
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((server_address, server_port))
 sock.listen(1)
 print('server is starting：waiting from clients')
@@ -42,7 +40,7 @@ while True:
         print(f'file size is{file_size}')
 
         current_usage = get_storage_usage(dir_path)
-        if current_usage + file_size > MAX_STORAGE:
+        if current_usage + file_size > max_storage:
             raise Exception('max storage over')
 
         if file_size == 0:
@@ -50,7 +48,6 @@ while True:
         
         # 次に、クライアントからファイル名を読み取り、変数に格納します。JSONデータがある場合は、サポートされていないため、例外が発生します。受信するデータがない場合、コードは例外を発生させます。
         filename = connection.recv(file_name_len).decode('utf-8')
-
         print('Filename: {}'.format(filename))
         
         # 次に、コードはクライアントから受け取ったファイル名で新しいファイルをフォルダに作成します。このファイルは、withステートメントを使用してバイナリモードで開かれ、write()関数を使用して、クライアントから受信したデータをファイルに書き込みます。データはrecv()関数を使用して塊単位で読み込まれ、データの塊を受信するたびにデータ長がデクリメントされます。
